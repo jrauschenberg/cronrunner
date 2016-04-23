f1eabf94b161822ed180bb568c4cafe9b0aeba9a
John Rauschenberg <john.rauschenberg@gmail.com>
2016-04-23 02:42:14 -0400

    Added chalk and diff modules.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var schedule = require('node-schedule');
 var fetch = require('node-fetch');
+var jsdiff = require('diff');
+var chalk = require('chalk');
 
-// var j = schedule.scheduleJob('* * * * * *', function(){
-//   console.log('The answer to life, the universe, and everything!');
-// });
 var page = {};
 var sites = ['http://cnn.com', 'http://google.com', 'http://purple.com'];
 sites.forEach(function(site) {
@@ -21,7 +20,15 @@ var siteChecker = function(site) {
     .then(function(res) {
       return res.text();
     }).then(function(body) {
-      if (page[site] !== body && page[site]) console.log(site, "has changed!");
-      page[site] = body;
+      if (page[site] !== body && page[site]) {
+        console.log(site, "has changed!");
+        var diff = jsdiff.diffLines(page[site], body);
+        diff.forEach(function(part) {
+          if (part.added) chalk.green(part);
+          if (part.removed) chalk.red(part);
+          console.log(part);
+        });
+        page[site] = body;
+      }
     });
 }; 
